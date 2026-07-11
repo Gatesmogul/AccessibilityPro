32a55bbb6d069539910687881b0368e4e0c1d6d1
Gatesmogul <ogunmola83@gmail.com>
2026-07-11 03:57:34 +0100

    Update firebaseAdmin.js

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,18 +1,13 @@
-const { initializeApp, cert, getApps } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
+const admin = require("firebase-admin");
 
-const app =
-  getApps().length === 0
-    ? initializeApp({
-        credential: cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        }),
-      })
-    : getApps()[0];
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
-module.exports = {
-  app,
-  auth: getAuth(app),
-};
+module.exports = admin;
